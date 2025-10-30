57a35c4ff25c73688ab30ca5a48db8e2f297610c
EricNahuel2002 <ericaquino2002@gmail.com>
2025-10-30 17:21:23 -0300

    fix al esquema para crear la base de datos para que sea mas flexible al agregar registros

--- src/repository/usuario-juego.repository.ts
+++ src/repository/usuario-juego.repository.ts
@@ -2,25 +2,25 @@ import { prisma } from "../prisma.js";
 
 export class UsuarioJuegoRepository {
   async crearRelacion(usuarioId: number, juegoId: number) {
-    return prisma.Usuario_Juego.create({ data: { usuarioId, juegoId, detalle: "" } });
+    return prisma.usuario_Juego.create({ data: { usuarioId, juegoId, detalle: "" } });
   }
 
   async obtenerJuegosDeUsuario(usuarioId: number) {
-    return prisma.Usuario_Juego.findMany({
+    return prisma.usuario_Juego.findMany({
       where: { usuarioId },
       include: { juego: true }
     });
   }
 
   async obtenerUsuariosDeJuego(juegoId: number) {
-    return prisma.Usuario_Juego.findMany({
+    return prisma.usuario_Juego.findMany({
       where: { juegoId },
       include: { usuario: true }
     });
   }
 
   async eliminarRelacion(usuarioId: number, juegoId: number) {
-    return prisma.Usuario_Juego.deleteMany({
+    return prisma.usuario_Juego.deleteMany({
       where: { usuarioId, juegoId }
     });
   }
